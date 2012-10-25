af582cde439ab2ff7e50b1dfedc3a9466befbe1f
Alexader Wagner <alexander_wagner@taylor.edu>
2012-10-24 21:38:23 -0400

    Strated the repair

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,5 @@
 #
 
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
-	
-	validates :name, presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-	validates :password, presence: true, length: { minimum: 6 }
-	validates :password_confirmation, presence: true
+  attr_accessible :name, :email
 end
